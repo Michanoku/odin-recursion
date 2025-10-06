4d3f8f968459076152a7ab3e7266144ad31abdb7
Michanoku <michael.werker@gmail.com>
2025-10-06 03:03:29 +0000

    Add regular fibonacci function

--- fibonacci.js
+++ fibonacci.js
@@ -1,9 +1,20 @@
 // The fibonacci function
-function fibs() {
-  // TODO
+function fibs(digits) {
+  const fibArray = new Array();
+  let current = 0;
+  let next = 1;
+  for (let i = 0; i < digits; i++) {
+    fibArray.push(current);
+    const temp = current;
+    current = next;
+    next+= temp;
+  }
+  return fibArray;
 }
 
 // The fibonacci function with recursion
 function fibsRec() {
   // TODO
 }
+
+console.log(fibs(8));
