ad4cb6b7900369ecd7611a9447ad35850b5fbe90
Michanoku <michael.werker@gmail.com>
2025-10-06 04:09:20 +0000

    Add function skeleton and test cases

---
+++ merge-sort.js
@@ -0,0 +1,11 @@
+// Merge sort function with recursion
+function mergeSort(array) {
+
+}
+
+// Tests
+console.log(mergeSort([]));
+console.log(mergeSort([73]));
+console.log(mergeSort([1, 2, 3, 4, 5]));
+console.log(mergeSort([3, 2, 1, 13, 8, 5, 0, 1]));
+console.log(mergeSort([105, 79, 100, 110]));
